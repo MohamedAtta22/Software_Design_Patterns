2e48fef1b6043df2809fb241fec1dd5a2cc04e72
MohamedAtta22 <109322691+MohamedAtta22@users.noreply.github.com>
2026-08-29 06:26:50 +0300

    ADD main.cpp for observer pattern#3

---
+++ mike_shah/the_observer_pattern3/main.cpp
@@ -0,0 +1,27 @@
+// Third Try
+#include<iostream>
+#include "IObserver.hpp"
+#include "ISubject.hpp"
+
+
+int main(){
+
+    SomeSubject subject;
+    Watcher Watcher1(subject, "Watcher-1");
+    Watcher Watcher2(subject, "Watcher-2");
+    {
+    Watcher Watcher3(subject, "Watcher-3");
+
+    // subject.AddObserver(&Watcher1);
+    // subject.AddObserver(&Watcher2);
+    // subject.AddObserver(&Watcher3);
+    }
+    subject.NotifyAll();
+
+    //subject.RemoveObserver(&Watcher3);
+    std::cout<<std::endl;
+
+    subject.NotifyAll();
+
+    return 0;
+}
